d901a0b2ae1dbf7e1209224ef4da8aa123d13aee
郑凡 <zhengfan@insta360.com>
2024-08-13 01:12:54 +0800

    fix

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -7,7 +7,6 @@ async function getData(lang) {
       `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
       {
         method: "GET",
-        body: JSON.stringify({ lang }),
       }
     );
     const data = await response.json();
