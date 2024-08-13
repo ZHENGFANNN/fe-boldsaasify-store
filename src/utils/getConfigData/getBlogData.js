e9fbb24a13131f6e57bf0bce4664f74ac7fe930f
郑凡 <zhengfan@insta360.com>
2024-08-14 01:16:16 +0800

    fix

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -15,7 +15,30 @@ async function getData(lang) {
 }
 
 async function getDataV2(lang) {
-  return await import(`@@/locale/blogData/${lang}.json`);
+  switch (lang) {
+    case "cn":
+      return await require(`@@/locale/blogData/cn.json`);
+    case "en":
+      return await require(`@@/locale/blogData/en.json`);
+    case "ja":
+      return await require(`@@/locale/blogData/ja.json`);
+    case "ko":
+      return await require(`@@/locale/blogData/ko.json`);
+    case "ru":
+      return await require(`@@/locale/blogData/ru.json`);
+    case "fr":
+      return await require(`@@/locale/blogData/fr.json`);
+    case "de":
+      return await require(`@@/locale/blogData/de.json`);
+    case "es":
+      return await require(`@@/locale/blogData/es.json`);
+    case "it":
+      return await require(`@@/locale/blogData/it.json`);
+    case "hk":
+      return await require(`@@/locale/blogData/hk.json`);
+    default:
+      return await require(`@@/locale/blogData/en.json`);
+  }
 }
 
 export default async function getBlogList(lang) {
