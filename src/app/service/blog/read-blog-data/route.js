e9f99589d2dc2703e5c2401f36ccf5e8cd578cf0
郑凡 <zhengfan@insta360.com>
2024-08-17 16:25:57 +0800

    fix

--- src/app/service/blog/read-blog-data/route.js
+++ src/app/service/blog/read-blog-data/route.js
@@ -1,45 +1,74 @@
 /** @format */
 
-export const runtime = "nodejs";
+// export const runtime = "edge";
+// export const fetchCache = "force-cache";
+
+// const fs = require("fs");
+// const qs = require("qs");
+
+// import path from "path";
+// import getLanguage from "@/config/LANGUAGE";
+
+// const languageList = getLanguage("list");
+// const localeCache = {};
+
+// function updateLocaleCache(lang) {
+//   const filePath = path.join(
+//     process.cwd(),
+//     "locale",
+//     "blogData",
+//     `${lang}.json`
+//   );
+//   const fileContents = fs.readFileSync(filePath, "utf8");
+//   try {
+//     const data = JSON.parse(fileContents);
+//     localeCache[lang] = data;
+//   } catch {
+//     localeCache[lang] = fileContents;
+//   }
+// }
+
+// export const updateData = () => {
+//   languageList.forEach((item) => {
+//     updateLocaleCache(item.value);
+//   });
+// };
+
+// // 初始化缓存
+// updateData();
+
+// export async function GET(req) {
+//   const { language } = qs.parse(req.url.split("?")[1]);
+//   console.log("[locale]: ", language);
+//   const data = Response.json(localeCache);
+//   return data;
+// }
+
+export const runtime = "edge"; // 修改为 'edge'
 export const fetchCache = "force-cache";
 
-const fs = require("fs");
-const qs = require("qs");
-
-import path from "path";
 import getLanguage from "@/config/LANGUAGE";
 
 const languageList = getLanguage("list");
-const localeCache = {};
 
-function updateLocaleCache(lang) {
-  const filePath = path.join(
-    process.cwd(),
-    "locale",
-    "blogData",
-    `${lang}.json`
-  );
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  try {
-    const data = JSON.parse(fileContents);
-    localeCache[lang] = data;
-  } catch {
-    localeCache[lang] = fileContents;
+const localeCache = {};
+async function loadLocaleData(language) {
+  if (!localeCache[language]) {
+    try {
+      const data = await import(`@@/locale/blogData/${language}.json`);
+      localeCache[language] = data.default;
+    } catch (error) {
+      console.error(`Error loading data for language ${language}:`, error);
+      localeCache[language] = {}; // 如果加载数据失败，设置为空对象
+    }
   }
+  return localeCache[language];
 }
 
-export const updateData = () => {
-  languageList.forEach((item) => {
-    updateLocaleCache(item.value);
-  });
-};
-
-// 初始化缓存
-updateData();
-
 export async function GET(req) {
-  const { language } = qs.parse(req.url.split("?")[1]);
+  const url = new URL(req.url);
+  const language = url.searchParams.get("language"); // 默认语言为 'en'
   console.log("[locale]: ", language);
-  const data = Response.json(localeCache);
-  return data;
+  const data = await loadLocaleData(language);
+  return Response.json(data);
 }
--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -20,17 +20,18 @@ async function getDataV3(lang) {
 }
 
 async function getData(lang) {
-  if (!localeData[lang]) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data?language=${lang}`,
-      {
-        method: "GET",
-      }
-    );
-    const data = await response.json();
-    localeData[lang] = data[lang];
-  }
-  return localeData[lang];
+  // if (!localeData[lang]) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data?language=${lang}`,
+    {
+      method: "GET",
+    }
+  );
+  const data = await response.json();
+  return data;
+  // localeData[lang] = data[lang];
+  // }
+  // return localeData[lang];
 }
 
 // async function getDataV2(lang) {
@@ -67,7 +68,6 @@ async function getData(lang) {
 export default async function getBlogList(lang) {
   const startTime = Date.now();
   const data = await getData(lang);
-
   console.log(`---获取Blog时间: ${Date.now() - startTime}---`);
   return data;
 }
