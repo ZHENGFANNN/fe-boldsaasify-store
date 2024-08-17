6d3e2d67ce537f1e11930b24f7c59335b563a402
郑凡 <zhengfan@insta360.com>
2024-08-17 17:23:30 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -30,12 +30,9 @@ const nextConfig = {
     ];
   },
 
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias["@"] = path.join(__dirname, "src");
     config.resolve.alias["@@"] = __dirname;
-
-    config.externals = config.externals || {};
-    config.externals["@@/locale/blogData"] = "commonjs @@/locale/blogData";
 
     return config;
   },
