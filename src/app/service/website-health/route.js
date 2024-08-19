ab8f7c93d6307612c67c5d03fb4687e0be402749
郑凡 <zhengfan@insta360.com>
2024-08-20 00:15:32 +0800

    fix

--- src/app/service/website-health/route.js
+++ src/app/service/website-health/route.js
@@ -2,7 +2,9 @@
 
 export async function GET() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/service-health`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/service-health`, {
+      cache: "no-cache",
+    });
     const { code } = await res.json();
     if (code === 0) {
       return Response.json({
