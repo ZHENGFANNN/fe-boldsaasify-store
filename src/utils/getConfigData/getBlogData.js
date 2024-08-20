f3b9433c118acb41201536a14f6b32117299a4c7
ZhengFan <53977416+ZHENGFANNN@users.noreply.github.com>
2024-08-20 14:21:36 +0800

    Update getBlogData.js

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -6,32 +6,33 @@ import { unstable_cache } from "next/cache";
 /** @format */
 const localeData = new Map();
 async function getData({ lang, area }) {
-  if (!localeData.get(`${lang}:${lang}`)) {
+  if (!localeData.get(`${lang}:${area}`)) {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data?language=${lang}&area=${area}`,
       { method: "GET" }
     );
     const data = await response.json();
-    localeData.set(`${lang}:${lang}`, data);
+    localeData.set(`${lang}:${area}`, data);
   }
-  return localeData.get(`${lang}:${lang}`);
+  return localeData.get(`${lang}:${area}`);
 }
 
-
-
-export default async function getBlogList(lang) {
-  const startTime = Date.now();
-  const area = cookies().get("area")?.value || "us";
- const getCachedData = unstable_cache(
-    async (lang, area) => {
+const getCachedData = function (lang, area) {
+  return unstable_cache(
+    async () => {
       const data = await getData({ lang, area });
       return data;
     },
     {
       key: `blog:${lang}:${area}`,
-      revalidate: 365 * 60 * 60 * 24, // 可选：设置缓存过期时间为24小时
     }
-  );
+)();
+}
+
+
+export default async function getBlogList(lang) {
+  const startTime = Date.now();
+  const area = cookies().get("area")?.value || "us";
 
   const data = await getCachedData(lang, area);
   console.log(`---获取Blog时间: ${Date.now() - startTime}---`);
